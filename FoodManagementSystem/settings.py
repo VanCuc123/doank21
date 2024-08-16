7551404816e5763dcf8649bd58a6782260430b52
VanCuc123 <89376090+VanCuc123@users.noreply.github.com>
2024-08-16 15:39:27 +0700

    1

--- FoodManagementSystem/settings.py
+++ FoodManagementSystem/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'fms',  
         'USER': 'root',  
         'PASSWORD': '',  
-        'HOST': 'localhost',  
+        'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
